5c17067d32c06b07da82360e019bdee99f8448e3
desktop-0q6ttnf\s.g <samat.gaynullin@yandex.ru>
2026-09-13 20:56:27 +0300

    Simple argument handle added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
+
+const std::string PROGRAM_NAME = ProgramName;
+const std::string PROGRAM_DESCRIPTION = ProgramDescription;
+const std::string PROGRAM_VERSION = VERSION;
 
 int main(int argc, char* argv[]) {
+    argparse::ArgumentParser program(PROGRAM_NAME,PROGRAM_VERSION);
+    program.add_description(PROGRAM_DESCRIPTION);
 
-    std::cout << "Test";
+    program.parse_args(argc, argv);
     return 0;
-
 }
