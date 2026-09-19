5d9375efbdfb3e0dbadd8c6960e6956da62f8a40
desktop-0q6ttnf\s.g <samat.gaynullin@yandex.ru>
2026-09-19 22:17:10 +0300

    Interactive class finished

---
+++ header/Interactive.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+//Класс для хранения интерактивных функций. Нужен для разбиения файла main.cpp
+class interactiveLogic {
+private:
+    template <typename InputValue>
+    static InputValue input(std::string msg, bool infinityLoop = false); 
+public:
+    
+    //функция для уточнение пользовательских путей и сохранение их 
+    static std::vector<std::filesystem::path> UserPathsAsk();
+
+};
+
+template <typename InputValue>
+inline InputValue interactiveLogic::input(std::string msg, bool infinityLoop)
+{
+    InputValue variable;
+    do {
+        try {
+            std::cout << std::endl << msg;
+            std::cin >> variable;
+        }
+        catch(...) {
+            std::cout << "\n\nERROR! Please enter correct value.";
+            std::cin.clear();
+
+            continue;
+        }
+
+        return variable; //Если ввод был успешен и код до сюда дошёл
+    }
+    while(infinityLoop); //Если флаг InfinityLoop включен - цикл будет повторяться до успешного ввода
+
+    return variable; //Заглушка чтобы убрать предупреждение
+}
